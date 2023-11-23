0b7d8cedeab3d26f89aa77bb3c3f36b543e4e04f
Ming Soon Tee <tee_ming_soon@hotmail.com>
2023-11-23 17:42:04 +0800

    Update MarkAttendance.js

--- src/pages/MarkAttendance.js
+++ src/pages/MarkAttendance.js
@@ -1,41 +1,42 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import InputId from "../components/InputId";
 import InputDate from "../components/InputDate";
 
 export default function MarkAttendance() {
     const [classId, setClassId] = useState('');
     const [classDate, setClassDate] = useState('');
-    const [students, setStudents] = useState([]);
+    const [roomNumber, setRoomNumber] = useState('');
+    const [students, setStudents] = useState([
+        { studentId: 'S001', name: 'John', attendance: 'N' },
+        { studentId: 'S002', name: 'Jane', attendance: 'N' },
+        // ... other students
+    ]);
 
-    // fetch from dbms with API endpoint
-    useEffect(() => {
-        if (classId && classDate) {
-            fetch(`http://localhost:3000/students?classId=${classId}&date=${classDate}`)
-                .then(response => response.json())
-                .then(data => setStudents(data.map(student => ({ ...student, isPresent: false }))))
-                .catch(error => console.error('Error fetching students:', error));
-        }
-    }, [classId, classDate]);
-
-    const handleAttendanceChange = (studentId, isPresent) => {
-        setStudents(students.map(student => 
-            student.id === studentId ? { ...student, isPresent: isPresent } : student
+    const handleAttendanceChange = (studentId, attendance) => {
+        setStudents(students.map(student =>
+            student.studentId === studentId ? { ...student, attendance: attendance ? 'Y' : 'N' } : student
         ));
     };
 
-    const submitAttendance = () => {
-        // Replace 'API_ENDPOINT' with your actual API endpoint for submitting attendance
-        fetch('API_ENDPOINT/submit-attendance', {
+    const handleSubmit = () => {
+        const attendanceData = {
+            classId,
+            classDate,
+            roomNumber,
+            students
+        };
+
+        fetch('API_ENDPOINT/submit-attendance', { // Replace API_ENDPOINT with your actual endpoint
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ classId, classDate, students }),
+            body: JSON.stringify(attendanceData)
         })
         .then(response => {
             if (response.ok) {
                 console.log('Attendance submitted successfully');
-                // Handle successful submission, maybe clear form or show a success message
+                // Additional actions upon successful submission (like showing a message)
             } else {
                 console.error('Failed to submit attendance');
                 // Handle errors
@@ -44,44 +45,50 @@ export default function MarkAttendance() {
         .catch(error => console.error('Error submitting attendance:', error));
     };
 
+    const shouldShowTable = classId && classDate && roomNumber;
+
     return (
         <div>
             <h1>Mark Attendance</h1>
             <div>
                 <InputId label='Class ID' value={classId} setValue={setClassId} />
-                <InputDate 
-                    label='Class Date' 
-                    minDate='2020-01-01'  // Adjust these dates as needed
-                    maxDate='2030-12-31'
-                    value={classDate} 
-                    setValue={setClassDate} 
-                />
+                <InputDate label='Class Date' minDate='2020-01-01' maxDate='2030-12-31' value={classDate} setValue={setClassDate} />
+                <InputId label='Room Number' value={roomNumber} setValue={setRoomNumber} />
             </div>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Student ID</th>
-                        <th>Name</th>
-                        <th>Attendance</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {students.map(student => (
-                        <tr key={student.id}>
-                            <td>{student.id}</td>
-                            <td>{student.name}</td>
-                            <td>
-                                <input 
-                                    type="checkbox" 
-                                    checked={student.isPresent} 
-                                    onChange={e => handleAttendanceChange(student.id, e.target.checked)} 
-                                />
-                            </td>
+            {shouldShowTable && (
+                <>
+                    <table border={'1'} style={{ width: '100%', position: "relative" }}>
+                    <thead>
+                        <tr>
+                            <th>Student ID</th>
+                            <th>Name</th>
+                            <th>Class ID</th>
+                            <th>Room Number</th>
+                            <th>Date</th>
+                            <th>Attendance Y/N</th>
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-            <button onClick={submitAttendance}>Submit Attendance</button>
+                    </thead>
+                    <tbody>
+                        {students.map((student, index) => (
+                            <tr key={index}>
+                                <td>{student.studentId}</td>
+                                <td>{student.name}</td>
+                                <td>{classId}</td>
+                                <td>{roomNumber}</td> {/* Display room number */}
+                                <td>{classDate}</td>
+                                <td>
+                                    <select value={student.attendance} onChange={e => handleAttendanceChange(student.studentId, e.target.value === 'Y')}>
+                                        <option value="Y">Y</option>
+                                        <option value="N">N</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                    </table>
+                    <button onClick={handleSubmit}>Submit Attendance</button>
+                </>
+            )}
         </div>
     );
 }
