b34fbfd2c0fd6719f9a277da2b83400a908008f1
Xwhc <150514197+Xwhc@users.noreply.github.com>
2023-11-26 23:15:28 +0800

    Update studentAttendance.js

--- backend/src/studentAttendance.js
+++ backend/src/studentAttendance.js
@@ -47,56 +47,60 @@ router.route('/')
             }
         })
     })
-    .post((req, res) => { // to create studentAttendance details
-        console.log('POST: /studentAttendance');
+    .post((req, res) => { //update
+        console.log('POST: /studentAttendance?ProfID=' + req.query.ProfID);
+      
         var StuID = req.body.StuID;
         var ClsID = req.body.ClsID;
         var Date = req.body.Date;
-        var Attendance = req.body.Attendance;        
-        models.studentAttendance.create({ StuID: StuID, ClsID: ClsID, Date:Date, Attendance: Attendance}).then(() => {
-            res.sendStatus(200);
-        }).catch(() => {
-            res.sendStatus(400);
-        })
-    })
-    .put((req, res) => { // to update studentAttendanceInfo
-        console.log('PUT: /studentAttendance');
-
-        var StuID = req.body.StuID;
-        var ClsID = req.body.ClsID;
-        var Date = req.body.Date;
-        var Attendance = req.body.Attendance;     
-
-        models.StudentAttendance.findByPk(StuID,ClsID,Date).then((studentAttendance) => {
+        var Attendance = req.body.Attendance;
+      
+        models.StudentAttendance
+          .findOne({
+            where: { StuID: StuID, ClsID: ClsID, Date: Date },
+          })
+          .then((studentAttendance) => {
             if (studentAttendance === null) {
-                res.sendStatus(404);
+              res.sendStatus(404);
+            } else {
+              studentAttendance.Attendance = Attendance;
+              studentAttendance.save().then(() => {
+                res.sendStatus(200);
+              });
             }
-            else {
-                studentAttendance.Attendance = Attendance;
-                studentAttendance.save().then(() => {
-                    res.sendStatus(200);
-                })
-            }
-        })
-    })
-    .delete((req, res) => { //delete studentAttendance
-        console.log('DELETE: /studentAttendance?StuID=&ClsID=&Date=' + req.query.StuID + req.query.ClsID + req.query.Date);
+          });
+      })
 
-        var StuID = req.body.StuID;
-        var ClsID = req.body.ClsID;
-        var Date = req.body.Date;
+    // .post((req, res) => { // to create studentAttendance details
+    //     console.log('POST: /studentAttendance');
+    //     var StuID = req.body.StuID;
+    //     var ClsID = req.body.ClsID;
+    //     var Date = req.body.Date;
+    //     var Attendance = req.body.Attendance;        
+    //     models.studentAttendance.create({ StuID: StuID, ClsID: ClsID, Date:Date, Attendance: Attendance}).then(() => {
+    //         res.sendStatus(200);
+    //     }).catch(() => {
+    //         res.sendStatus(400);
+    //     })
+    // })
+    // .delete((req, res) => { //delete studentAttendance
+    //     console.log('DELETE: /studentAttendance?StuID=&ClsID=&Date=' + req.query.StuID + req.query.ClsID + req.query.Date);
 
-        models.StudentAttendance.findByPk(StuID,ClsID,Date).then((studentAttendance) => {
-            if (studentAttendance === null) {
-                res.sendStatus(404);
-            }
-            else {
-                studentAttendance.destroy().then(() => {
-                    res.sendStatus(200);
-                })
-            }
-        })
-    });
+    //     var StuID = req.body.StuID;
+    //     var ClsID = req.body.ClsID;
+    //     var Date = req.body.Date;
+
+    //     models.StudentAttendance.findByPk(StuID,ClsID,Date).then((studentAttendance) => {
+    //         if (studentAttendance === null) {
+    //             res.sendStatus(404);
+    //         }
+    //         else {
+    //             studentAttendance.destroy().then(() => {
+    //                 res.sendStatus(200);
+    //             })
+    //         }
+    //     })
+    // });
 
 
 module.exports = router;
