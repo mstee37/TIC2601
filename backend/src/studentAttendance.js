6281fade32b8aeb8aec8076c41bb3f8855b0a3c7
Xwhc <150514197+Xwhc@users.noreply.github.com>
2023-11-28 16:56:05 +0800

    Update studentAttendance.js

--- backend/src/studentAttendance.js
+++ backend/src/studentAttendance.js
@@ -8,19 +8,17 @@ const router = express.Router()
 
 router.route('/')
     .get((req, res) => { // to get studentAttendance List
-        console.log('GET: /studentAttendance?ProfID=&ClsID=&Date=&RoomNo' + req.query.ProfID + req.query.ClsID + req.query.Date+ req.query.RoomNo);
+        console.log('GET: /studentAttendance?ProfID=&ClsID=&Date=' + req.query.ProfID + req.query.ClsID + req.query.Date+ req.query.RoomNo);
         var ProfIDs = req.query.ProfID; 
         var ClsIDs = req.query.ClsID; 
         var Dates=req.query.Date;
         var RoomNos=req.query.RoomNo;
         models.StudentAttendance.findAll({
             attributes: [
-              'ClsID','StuID',[models.sequelize.literal('"Student"."SName"'), 'Name'],[models.sequelize.literal('"Class"."RoomNo"'), 'RoomNo'],'Date','Attendance'],
+              'ClsID','StuID',[models.sequelize.literal('"Student"."SName"'), 'Name'],'Date','Attendance'],
             include: [{
                 model: models.Classes,
                 attributes: [],
-                where: { RoomNo: RoomNos },
-                as: 'Class',
                 include: {
                   model: models.Module,
                   attributes: [],
@@ -39,7 +37,7 @@ router.route('/')
             raw: true,
           })
           .then((classlist) => {
-            // console.log('Classlist:', classlist);
+            console.log('Classlist:', classlist);
             if (classlist === null) {
                 res.sendStatus(404);
             } else {
@@ -70,19 +68,6 @@ router.route('/')
             }
           });
       })
-
-    // .post((req, res) => { // to create studentAttendance details
-    //     console.log('POST: /studentAttendance');
-    //     var StuID = req.body.StuID;
-    //     var ClsID = req.body.ClsID;
-    //     var Date = req.body.Date;
-    //     var Attendance = req.body.Attendance;        
-    //     models.studentAttendance.create({ StuID: StuID, ClsID: ClsID, Date:Date, Attendance: Attendance}).then(() => {
-    //         res.sendStatus(200);
-    //     }).catch(() => {
-    //         res.sendStatus(400);
-    //     })
-    // })
     // .delete((req, res) => { //delete studentAttendance
     //     console.log('DELETE: /studentAttendance?StuID=&ClsID=&Date=' + req.query.StuID + req.query.ClsID + req.query.Date);
 
@@ -104,3 +89,4 @@ router.route('/')
 
 
 module.exports = router;
+
