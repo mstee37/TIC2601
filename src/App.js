69327e6801b40a2aa0437ca90a7f1fd0eec45a51
Joseph Lim <joseph98.work@gmail.com>
2023-11-29 19:27:04 +0800

    deleted unused page
    deleted unused page

--- src/App.js
+++ src/App.js
@@ -6,9 +6,6 @@ import { UserContext } from "./contexts/UserContext";
 import Layout from "./pages/Layout";
 import Index from "./pages/Index";
 import Error from "./pages/Error";
-import LoginPageStudent from "./pages/LoginPageStudent";
-import LoginPageProf from "./pages/LoginPageProf";
-import LoginPageAdmin from "./pages/LoginPageAdmin";
 import MainPageStudent from "./pages/MainPageStudent";
 import MainPageProf from "./pages/MainPageProf";
 import MainPageAdmin from "./pages/MainPageAdmin";
@@ -19,7 +16,6 @@ import SendFeedback from "./pages/Feedback";
 import MarkAttendance from "./pages/MarkAttendance";
 import CourseCreation from "./pages/CourseCreation";
 import Transcript from "./pages/Transcript";
-import LoginPage from "./pages/LoginPage";
 import Login from "./pages/Login";
 import Grading from "./pages/Grading";
 import Enrollment from "./pages/Enrollment";
@@ -40,9 +36,6 @@ function App() {
           <Route index element={<Login/>} />
           <Route path="Index" element={<Index />} />
           <Route path="Login" element={<Login />} />
-          <Route path="LoginPageStudent" element={<LoginPageStudent />} />
-          <Route path="LoginPageProf" element={<LoginPageProf />} />
-          <Route path="LoginPageAdmin" element={<LoginPageAdmin />} />
           <Route path="MainPageStudent" element={<MainPageStudent />} />
           <Route path="MainPageProf" element={<MainPageProf />} />
           <Route path="MainPageAdmin" element={<MainPageAdmin />} />
